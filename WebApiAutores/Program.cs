e740d955b194185c75604f5332356fd0e1edc39b
Agustina Gallardo <agusgallardo275@gmail.com>
2022-12-28 16:45:09 -0300

    ControllerAutores

---
+++ WebApiAutores/Program.cs
@@ -0,0 +1,13 @@
+using WebApiAutores;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startUp = new StartUp(builder.Configuration);
+
+startUp.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startUp.Configure(app,app.Environment);
+
+app.Run();
---
+++ WebApiAutores/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace WebApiAutores
+{
+    public class StartUp
+    {
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get;  }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints( Endpoint =>
+                {
+                Endpoint.MapControllers();
+            });
+
+        }
+    }
+}
