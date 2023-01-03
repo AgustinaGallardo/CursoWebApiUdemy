5993ccb5f653990aa199ec78e7406f4556fa0734
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-03 13:06:19 -0300

    BaseDeDatos

--- WebApiAutores/StartUp.cs
+++ WebApiAutores/StartUp.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApiAutores
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiAutores
 {
     public class StartUp
     {
@@ -13,6 +15,10 @@
         {
 
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
