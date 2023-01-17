6f50483a2fb3ef5ee721487b763a3cfb02def1c0
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-17 10:16:00 -0300

    ControllerLibros-get-post

--- WebApiAutores/ApplicationDbContext.cs
+++ WebApiAutores/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace WebApiAutores
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
