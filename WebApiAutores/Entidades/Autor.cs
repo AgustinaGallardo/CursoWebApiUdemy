7bf71231f57e1d9320f27a7238e933b0f217ae35
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-18 10:15:36 -0300

    validacion por atributo

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.Validaciones;
 
 namespace WebApiAutores.Entidades
 {
@@ -8,6 +9,7 @@ namespace WebApiAutores.Entidades
         public int Id { get; set; }
         [Required(ErrorMessage = "El {0} es requerido para ingresar un nuevo autor")] // Define al nombre como requerido
         [StringLength(maximumLength:5,ErrorMessage = "El campo {0} no puede tener mas de {1} letras")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         [Range(18,120)] //valida q la edad tenga un rango de 18 a 120 años
         [NotMapped]
